c896a586a018509e6ded54a791cf6cc0981cd8f6
corazzon <joeunpark@gmail.com>
2017-12-17 18:21:06 +0900

    chapter15 docstrings

---
+++ O’Reilly_jpub_learning_python/docstrings.py
@@ -0,0 +1,21 @@
+"""
+Module documentation
+Words Go Here
+"""
+spam = 40
+
+def square(x):
+    """
+    fuction documentation
+    can we have your liver then?
+    간 좀 빼가도 되겠니???
+    왜 이런 생뚱맞은 질문이 있을까?
+    """
+    return x ** 2
+
+class Employee:
+    "class documentation"
+    pass
+
+print(square(4))
+print(square.__doc__)
